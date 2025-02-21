cb35e4f6281378175d1fd23a4f4b0a9644974423
Eduardo Costa <eduardoengcosta@gmail.com>
2025-02-21 14:56:40 -0300

    Teste - CodGo.X

---
+++ CEP/CEP.dpr
@@ -0,0 +1,14 @@
+program CEP;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {FormPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.Run;
+end.
